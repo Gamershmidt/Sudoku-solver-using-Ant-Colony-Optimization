42d5bb5352a1246e62579c725e1aab1fb792f930
Arina Goncharova <a.goncharova@innopolis.university>
2024-04-08 20:37:49 +0300

    base algorithm

---
+++ src/Ant.py
@@ -0,0 +1,60 @@
+import numpy as np
+from SudokuGrid import Grid
+
+class Ant:
+    def __init__(self, grid= None, pheromone_matrix = None, row = 0, column = 0, default_pheromone = 1/81, local_evaporation_rate = 0.01):
+        if not grid:
+            self.board_size = 81
+        else:
+            self.board_size = grid.size
+        self.grid = grid
+        self.num_of_fixed = 0
+        self.num_of_incorrect = 0
+        self.pheromone_matrix = pheromone_matrix
+        self.row = row
+        self.column = column
+        self.default_pheromone = default_pheromone
+        self.local_evaporation_rate = local_evaporation_rate
+        self.num_of_visited = 0
+
+    def perform_move(self):
+        self.move_one_cell()
+        self.num_of_visited += 1
+        cur_cell_possible_values = self.grid.get_possible_values(self.row, self.column)
+
+        if self.grid.check_fixed_cell(self.row, self.column):
+            self.num_of_fixed += 1
+        elif len(cur_cell_possible_values) == 0:
+            self.num_of_incorrect += 1
+        else:
+            # choose value from current cell’s value set;
+            pheromones_sum = 0
+            for val in cur_cell_possible_values:
+                pheromones_sum += self.pheromone_matrix[self.row][self.column][val-1]
+            probabilities = [self.pheromone_matrix[self.row][self.column][cur_cell_possible_values[i] - 1] / pheromones_sum for i in range(len(cur_cell_possible_values))]
+            chosen_value = np.random.choice(cur_cell_possible_values, p=probabilities)
+
+            # fix value
+            self.grid.sudoku[self.row][self.column].fix_cell(chosen_value)
+            self.num_of_fixed += 1
+
+            # propagate constraints
+            self.grid.update_cell_values(self.row, self.column)
+
+            # local pheromone update
+            self.pheromone_matrix[self.row][self.column][chosen_value - 1] = (
+                    (1 - self.local_evaporation_rate) * self.pheromone_matrix[self.row][self.column][
+                chosen_value - 1] + self.local_evaporation_rate * self.default_pheromone)
+
+    def move_one_cell(self):
+        if self.column+1 < self.board_size:
+            self.column += 1
+        elif self.row + 1 < self.board_size:
+            self.row += 1
+            self.column = 0
+        elif self.row == 8 and self.column == 8 and self.num_of_visited < 81:
+            self.row = 0
+            self.column = 0
+
+    def get_f(self):
+        return self.num_of_fixed - self.num_of_incorrect
--- src/SudokuGrid.py
+++ src/SudokuGrid.py
@@ -8,25 +8,67 @@ class Grid:
     def finalized(self):
         for i in range(self.size):
             for j in range(self.size):
-                if not self.sudoku[i][j].fixed():
+                if not self.sudoku[i][j].is_cell_fixed():
                     return False
 
         return True
 
     def set_cell(self, i, j, value):
-        self.sudoku[i][j].set_values(value)
+        self.sudoku[i][j].set_cell(value)
+    def fix_cell(self, i, j, value):
+        if not self.sudoku[i][j].is_cell_incorrect():
+            self.sudoku[i][j].fix_cell(value)
 
     def update_cell_values(self, pos_i, pos_j):
-        if not self.sudoku[pos_i][pos_j].check_fixed():
+        if not self.sudoku[pos_i][pos_j].is_cell_fixed():
             return
         for i in range(self.size):
             if i != pos_i:
-                self.sudoku[i][pos_j].drop_possible(self.sudoku[i][pos_j].value)
-                if self.sudoku[i][pos_j].check_fixed():
-                    self.update_cell_values(i, pos_j)
+                self.sudoku[i][pos_j].drop_possible(self.sudoku[pos_i][pos_j].value)
+
         for j in range(self.size):
             if j != pos_j:
-                self.sudoku[pos_i][j].drop_possible(self.sudoku[pos_i][j].value)
-                if self.sudoku[pos_i][j].check_fixed():
-                    self.update_cell_values(pos_i, j)
+                self.sudoku[pos_i][j].drop_possible(self.sudoku[pos_i][pos_j].value)
+
+        row_square = pos_i // self.dimension
+        column_square = pos_j // self.dimension
+        start_i = row_square * self.dimension
+        start_j = column_square * self.dimension
+
+        for i in range(start_i, start_i + self.dimension):
+            for j in range(start_j, start_j + self.dimension):
+                if (i != pos_i or j != pos_j):
+                    self.sudoku[i][j].drop_possible(self.sudoku[pos_i][pos_j].value)
+
         # add checker for small squares
+    def propagate_new_constraint(self, pos_i, pos_j):
+        for i in range(self.size):
+            if i != pos_i:
+                self.sudoku[i][pos_j].drop_possible(self.sudoku[pos_i][pos_j].value)
+        for j in range(self.size):
+            if j != pos_j:
+                self.sudoku[pos_i][j].drop_possible(self.sudoku[pos_i][pos_j].value)
+                # add checker for small squares
+                row_square = pos_i // self.dimension
+                column_square = pos_j // self.dimension
+                start_i = row_square * self.dimension
+                start_j = column_square * self.dimension
+        for i in range(start_i, start_i + self.dimension):
+            for j in range(start_j, start_j + self.dimension):
+                if (i != pos_i or j != pos_j):
+                    self.sudoku[i][j].drop_possible(self.sudoku[pos_i][pos_j].value)
+
+    def print_grid(self):
+        print(f"Size: ", len(self.sudoku[0]))
+        for i in range(len(self.sudoku[0])):
+            for j in range(len(self.sudoku[1])):
+                print(self.sudoku[i][j].value, end=' ')
+            print()
+
+    def get_cell_from_grid(self, i, j):
+        return self.sudoku[i][j]
+    def get_possible_values(self, row, column):
+        return self.sudoku[row][column].get_possible_values()
+
+    def check_fixed_cell(self, row, column):
+        return self.sudoku[row][column].is_cell_fixed()
