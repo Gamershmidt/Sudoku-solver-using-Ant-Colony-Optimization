33388117e22a8d73d0c4b04c4e8f5056a5b713ce
Rufina Gafiiatullina <r.gafiiatullina@innopolis.university>
2024-04-22 15:33:14 +0300

    Update the user input

--- src/GridShowing.py
+++ src/GridShowing.py
@@ -90,9 +90,9 @@ def initialize_grid():
 
 
 # Function to create stop button
-def create_stop_button(screen):
+def create_stop_button(screen, text):
     button_font = pygame.font.Font(None, FONT_SIZE)
-    button_text = button_font.render("Stop", True, BUTTON_TEXT_COLOR)
+    button_text = button_font.render(text, True, BUTTON_TEXT_COLOR)
     button_rect = button_text.get_rect(center=(WIDTH // 2, GRID_HEIGHT + (HEIGHT - WIDTH) // 2))
     pygame.draw.rect(screen, BUTTON_COLOR, button_rect)
     screen.blit(button_text, button_rect)
@@ -107,10 +107,11 @@ def main():
 
     screen.fill(WHITE)
     initial_grid = initialize_grid()
-    stop_button_rect = create_stop_button(screen)
 
     while True:
+        screen.fill(WHITE)
         draw_grid(screen)
+        stop_button_rect = create_stop_button(screen, "Confirm")
         draw_numbers(initial_grid, screen)
         if handle_events(initial_grid, stop_button_rect):
             break
@@ -126,9 +127,7 @@ def main():
     input_file = open("input.txt", "w")
     input_file.write(input_sudoku)
     input_file.close()
-    time.sleep(2)
-    # 001520040600100009040800070090000001008000400400000090070009080500008004010052600
-    # 001520040600100009040800070090000001008000400400000090070009080500008004010052600
+    time.sleep(1)
 
     AntColony.solve_one_puzzle('input.txt')
     grids = []
@@ -138,56 +137,7 @@ def main():
             for i in range(9):
                 temp.append(line[i*9:(i+1)*9])
             grids.append(temp)
-                #grids[-1].append(list(line)[:81][i: i + 9])
 
-    #print("Sudoku grid:", input_sudoku)
-    # grids = [
-    #     [[2, 1, 3, 8, 7, 9, 4, 6, 5],
-    #      [6, 5, 7, 3, 5, 1, 8, 8, 8],
-    #      [4, 9, 8, 8, 6, 4, 9, 4, 2],
-    #      [3, 5, 1, 9, 4, 1, 6, 6, 6],
-    #      [5, 4, 4, 1, 8, 8, 4, 2, 9],
-    #      [1, 3, 4, 2, 6, 6, 8, 6, 8],
-    #      [8, 5, 2, 2, 6, 6, 1, 5, 6],
-    #      [7, 4, 4, 6, 5, 1, 3, 2, 8],
-    #      [9, 9, 3, 9, 3, 2, 7, 4, 9]],
-    #     [[1, 8, 4, 7, 4, 1, 9, 9, 6],
-    #      [3, 8, 9, 5, 7, 2, 6, 1, 4],
-    #      [5, 6, 9, 3, 1, 5, 9, 8, 4],
-    #      [9, 6, 8, 6, 1, 4, 9, 5, 3],
-    #      [6, 5, 8, 5, 6, 5, 7, 4, 6],
-    #      [3, 9, 7, 8, 1, 2, 5, 4, 6],
-    #      [5, 2, 8, 4, 7, 4, 3, 9, 8],
-    #      [1, 3, 4, 2, 5, 3, 4, 7, 6],
-    #      [2, 8, 9, 3, 2, 8, 5, 4, 8]],
-    #     [[6, 5, 9, 4, 2, 1, 1, 6, 1],
-    #      [3, 5, 2, 7, 2, 5, 9, 3, 7],
-    #      [2, 8, 5, 9, 1, 4, 6, 3, 7],
-    #      [9, 1, 6, 6, 7, 9, 4, 9, 2],
-    #      [8, 3, 7, 2, 4, 1, 8, 3, 4],
-    #      [6, 8, 9, 2, 5, 1, 6, 7, 3],
-    #      [6, 5, 2, 4, 3, 6, 9, 5, 6],
-    #      [2, 7, 8, 6, 1, 5, 5, 5, 5],
-    #      [2, 7, 9, 6, 4, 1, 9, 2, 2]],
-    #     [[1, 9, 7, 2, 1, 4, 9, 5, 7],
-    #      [7, 8, 7, 4, 5, 4, 7, 2, 1],
-    #      [9, 9, 5, 2, 1, 4, 2, 2, 6],
-    #      [7, 4, 9, 8, 9, 4, 6, 1, 2],
-    #      [4, 9, 4, 5, 1, 3, 7, 7, 3],
-    #      [6, 5, 5, 3, 1, 4, 8, 1, 5],
-    #      [7, 1, 8, 5, 8, 8, 2, 8, 7],
-    #      [9, 1, 9, 6, 6, 4, 9, 1, 6],
-    #      [7, 1, 9, 6, 9, 5, 2, 3, 5]],
-    #     [[3, 9, 3, 6, 6, 9, 5, 5, 1],
-    #      [9, 4, 3, 9, 4, 2, 2, 8, 9],
-    #      [4, 8, 6, 7, 8, 6, 1, 1, 1],
-    #      [1, 8, 6, 4, 6, 3, 1, 5, 1],
-    #      [5, 2, 6, 5, 7, 9, 7, 5, 3],
-    #      [3, 1, 3, 8, 6, 8, 4, 9, 8],
-    #      [5, 9, 1, 3, 4, 1, 9, 2, 2],
-    #      [6, 3, 1, 4, 1, 7, 3, 5, 1],
-    #      [6, 1, 1, 1, 8, 4, 8, 7, 1]]
-    # ]
     grid_index = 0
     while True:
         screen.fill(WHITE)
@@ -206,12 +156,38 @@ def main():
         grid_index = (grid_index + 1)
 
         # Pause for 1 second
-        time.sleep(0.5)
+        time.sleep(0.3)
         if grid_index == len(grids):
             break
-    pygame.quit()
+    stop_button_rect = create_stop_button(screen, "Exit")
+
+    quit_point = False
+    while True:
+        draw_grid(screen)
+        draw_numbers(initial_grid, screen)
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                if stop_button_rect.collidepoint(mouse_pos):
+                    quit_point = True
+                    break
+        if quit_point:
+            pygame.quit()
+            sys.exit()
+        pygame.display.flip()
+        clock.tick(30)
 
 
 if __name__ == "__main__":
     main()
+
+# 050083017
+# 000100400
+# 304005608
+# 000030009
+# 090824500
+# 006000070
+# 009000050
+# 007290086
+# 103607204
 
